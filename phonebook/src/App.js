a3bc49696309c168d394bd2e83dbeab6821420b4
iamsuigeneris <irewolelanre@gmail.com>
2020-01-22 22:27:43 +0100

    2.9: The Phonebook Step4

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,22 +2,33 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-    const [ persons, setPersons] = useState([])
+
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+      ])
+
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
-
-    const rows = () => persons.map(person => 
+    const [ searchTerm, setSearchTerm ] = useState('')
+   
+    const rows = () => display.map(person => 
         <Person key={person.id} person={person} />
         
     )
+
+    const display = searchTerm
+        ? persons.filter(person => (person.name).toLowerCase().includes(searchTerm))
+        : persons ;
   
     const addName = (event) => {
         event.preventDefault()
 
         const personObject = {
-            id:persons.length + 1,
             name: newName,
-            number:newNumber
+            number: newNumber
         }
 
         let people = persons.map(person => person.name)
@@ -26,20 +37,27 @@ const App = () => {
         }else{
             setPersons(persons.concat(personObject))
         }            
-        // setNewName('')
-        // setNewNumber('')
+        setNewName('')
+        setNewNumber('')
     }
-
+    
     const handleNameChange = (event) => {
-            setNewName(event.target.value)       
+        setNewName(event.target.value)       
     }
     const handleNumberChange = (event) => {
-            setNewNumber(event.target.value)
+        setNewNumber(event.target.value)
+    }
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value)
     }
 
     return(
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input value={searchTerm} onChange={handleSearchChange}/>
+            </div>
+            <h2>add a new  </h2>
             <form onSubmit={addName}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange}/>
