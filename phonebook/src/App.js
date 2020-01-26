7dc0e465beb904715baf78af91e7d0f2fc1c045a
iamsuigeneris <irewolelanre@gmail.com>
2020-01-26 20:26:54 +0100

    2.11: The Phonebook Step6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import Form from './components/Form'
 import Filters from './components/Filters'
 
 const App = () => {
 
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ])
-
+    const [persons, setPersons] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ searchTerm, setSearchTerm ] = useState('')
    
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then( response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+
     const rows = () => display.map(person => 
-        <Person key={person.id} person={person} />
-        
+        <Person key={person.id} person={person} /> 
     )
     
     const display = searchTerm
